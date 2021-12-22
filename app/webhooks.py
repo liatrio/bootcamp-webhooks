e465433844b56d740e4301288463cc47ed3c3deb
ssmathistad <ssmathistad@mail.csuchico.edu>
2021-12-22 14:28:14 -0800

    feat: update lesson for bootcamp

--- app/webhooks.py
+++ app/webhooks.py
@@ -5,3 +5,20 @@ import base64
 
 warden = Flask(__name__)
 
+#POST route for Admission Controller  
+@warden.route('/validate', methods=['POST'])
+#Admission Control Logic - validating
+def validating_webhook():
+    request_info = request.get_json()
+    uid = request_info["request"].get("uid")
+
+    # Code for validating webhook HERE
+
+#POST route for Admission Controller
+@warden.route("/mutate", methods=["POST"])
+#Admission Control Logic - mutating
+def mutatating_webhook():
+    request_info = request.get_json()
+    uid = request_info["request"].get("uid")
+
+    # Code for mutating webhook HERE
